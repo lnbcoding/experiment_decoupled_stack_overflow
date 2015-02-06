0f720b1a7db8e4c7f5b177118505f65c5a3e8679
lnbcoding <leonardbui@gmail.com>
2015-02-06 15:44:42 -0800

    remove duplicated back end server dirss

--- decoupled_stack_overflow/app/controllers/answers_controller.rb
+++
@@ -1,6 +0,0 @@
-class AnswersController < ApplicationController
-    belongs_to :question
-
-    validates :title, presence: true
-    validates :content, presence: true
-end
--- decoupled_stack_overflow/app/models/question.rb
+++
@@ -1,6 +0,0 @@
-class Question < ActiveRecord::Base
-    has_many :answers
-
-    validates :title, presence: true
-    validates :content, presence: true
-end
