fb3fdcbe7501f8b1085b9b2d2943aa87ceea7a2c
lnbcoding <leonardbui@gmail.com>
2015-02-06 15:18:56 -0800

    add CORS private method in questions_controller

--- decoupled_stack_overflow/app/controllers/questions_controller.rb
+++ decoupled_stack_overflow/app/controllers/questions_controller.rb
@@ -1,4 +1,7 @@
 class QuestionsController < ApplicationController
+
+    after_filter :cors_set_access_control_headers
+
     def index
         @questions = Question.all
         render json: @questions
@@ -8,4 +11,12 @@ class QuestionsController < ApplicationController
         @question = Question.find(params[:id])
         render json: @question
     end
+
+    private
+        def cors_set_access_control_headers
+            headers['Access-Control-Allow-Origin'] = '*'
+            headers['Access-Control-Allow-Methods'] = 'POST, GET, PUT, DELETE, OPTIONS'
+            headers['Access-Control-Allow-Headers'] = 'Origin, Content-Type, Accept, Authorization, Token'
+            headers['Access-Control-Max-Age'] = "1728000"
+        end
 end
