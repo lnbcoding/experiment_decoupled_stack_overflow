9348af8dac25d15dfe1d1198641293a9686f1473
lnbcoding <leonardbui@gmail.com>
2015-02-06 17:23:07 -0800

    clean up JS file for the ajax

--- front_end_sinatra/public/js/application.js
+++ front_end_sinatra/public/js/application.js
@@ -1,23 +1,22 @@
 $(document).ready(function() {
 
+    // Get All Question using Ajax and rendering Client Side
     $.ajax({
         url: 'http://localhost:3000/questions',
         type: 'GET',
-        dataType: 'JSON'
     }).done(function(serverResponse) {
+        // iterate over the array of Objects and display the title and content
         for (var i = 0; i < serverResponse.length; i ++) {
-            $("table").append('<tr><td>' + serverResponse[i].title + '</td> <td> ' + serverResponse[i].content + ' </td></tr>');
+            $("table.each-question-index").append('<tr><td>' + serverResponse[i].title + '</td> <td> ' + serverResponse[i].content + ' </td></tr>');
         }
     });
 
+    // Get One Question using Ajax and rendering Client Side
     $.ajax({
         url: 'http://localhost:3000/questions',
         type: 'GET',
-        dataType: 'JSON'
     }).done(function(serverResponse) {
-        for (var i = 0; i < serverResponse.length; i ++) {
-            $("table").append('<tr><td>' + serverResponse[i].title + '</td> <td> ' + serverResponse[i].content + ' </td></tr>');
-        }
+        $("table.one-question").append('<tr><td>' + serverResponse.title + '</td> <td> ' + serverResponse.content + ' </td></tr>');
     });
 
 });
