f1b18be745af31524549fe68663de8b33ac8cb76
lnbcoding <leonardbui@gmail.com>
2015-02-06 17:06:02 -0800

    add ajax calls for the client-side rendering

--- front_end_sinatra/public/js/application.js
+++ front_end_sinatra/public/js/application.js
@@ -1,7 +1,23 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+    $.ajax({
+        url: 'http://localhost:3000/questions',
+        type: 'GET',
+        dataType: 'JSON'
+    }).done(function(serverResponse) {
+        for (var i = 0; i < serverResponse.length; i ++) {
+            $("table").append('<tr><td>' + serverResponse[i].title + '</td> <td> ' + serverResponse[i].content + ' </td></tr>');
+        }
+    });
+
+    $.ajax({
+        url: 'http://localhost:3000/questions',
+        type: 'GET',
+        dataType: 'JSON'
+    }).done(function(serverResponse) {
+        for (var i = 0; i < serverResponse.length; i ++) {
+            $("table").append('<tr><td>' + serverResponse[i].title + '</td> <td> ' + serverResponse[i].content + ' </td></tr>');
+        }
+    });
+
 });
